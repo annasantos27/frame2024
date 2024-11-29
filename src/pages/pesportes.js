d3bceff9f999bcb423c18dabfd397c9c242857dc
anna <anna.santos5@estudante.ifms.edu.br>
2024-11-29 07:51:28 -0300

    tabela aula 29-11

--- src/pages/pesportes.js
+++ src/pages/pesportes.js
@@ -1,18 +1,37 @@
-import { Container, TabContainer } from "react-bootstrap";
+import { Container, Table,  } from "react-bootstrap";
 import Footer from "./modulos/footer";
 import Menu from "./modulos/navbar";
 
-export default function Pesportes(){ 
-    return<>
-    <Menu/>
+export default function Pesportes() {
+    return <>
+        <Menu />
         <Container>
-           <Container className= "border border-succsess rounded-pill text-center col-sm-6">
-            Lista de Produtos de Esportes
+            <Container className="border border-succsess rounded-pill text-center col-sm-6">
+                Lista de Produtos de Esportes
             </Container>
             <Container>
-            Os Produtos
-            </Container> 
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Produto</th>
+                            <th>Categoria</th>
+                            <th>Descrição</th>
+                            <th>Preço</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </Container>
         </Container>
-        <Footer/>
-        </>
+        <Footer />
+    </>
 }
